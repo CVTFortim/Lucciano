da8cba9158a6aefb32ad9c9ce43aaf9012df7f9d
CVTFortim <145070302+CVTFortim@users.noreply.github.com>
2023-09-19 17:13:27 -0300

    Publicação do Site

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
+
+$('.variable-width').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
+       
